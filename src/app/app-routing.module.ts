a4d637f96bfab40c76384d681d04c011fab4583b
Torralba <andres.jr.r.torralba@accenture.com>
2020-02-01 19:25:10 +0800

    Login UI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
 
 const routes: Routes = [
-  { path: '', loadChildren: () => import('./layout/layout.module').then(m => m.LayoutModule) }
+  { path: 'home', loadChildren: () => import('./layout/layout.module').then(m => m.LayoutModule) },
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' }
 ];
 
 @NgModule({
